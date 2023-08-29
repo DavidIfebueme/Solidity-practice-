9f9fcf72f5689bb196a2230be1ca5a207fb80694
DavidIfebueme <davidifebueme@gmail.com>
2023-08-29 18:18:22 +0100

    Used call instead of transfer and send to withdraw funds from the contract in FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -12,6 +12,12 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         
         require(msg.value.getConversionRate() >= minimumUsd, "Not enough eth"); // at this point the msg,value is in eth and the minimumUsd is in usd, we're gonna have to fix that
@@ -19,14 +25,21 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
 
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
     }
 
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Must be owner!");
+        _;
+    }
    
 }
